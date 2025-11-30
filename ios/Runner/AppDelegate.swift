637dde974c6f6afc5a35011934b7b10ec7fa8a99
DELL <muhammad.adel.abualhaijaa@gmail.com>
2025-11-30 04:07:16 +0300

    Update iOS Firebase configuration and push notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,30 +1,13 @@
-import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
+import UIKit
 
-@main // ✅ فقط هذا
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    // تهيئة Firebase
-    FirebaseApp.configure()
-
-    // تسجيل الـ plugins
     GeneratedPluginRegistrant.register(with: self)
-
-    // تسجيل الإشعارات البعيدة
-    application.registerForRemoteNotifications()
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // الحصول على Device Token الخاص بـ APNs
-  override func application(_ application: UIApplication,
-      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
   }
 }
