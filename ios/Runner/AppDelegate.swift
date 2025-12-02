d38cddf5b2a13c32b56154a7eb3722e2e7ae8819
abwalhyja607 <abwalhyja607@gmail.com>
2025-12-03 01:51:03 +0300

    Initial commit - Flutter project with Firebase Notification setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,73 +1,13 @@
-import UIKit
 import Flutter
-import FirebaseCore
-import FirebaseMessaging
+import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    FirebaseApp.configure()
-
-    UNUserNotificationCenter.current().delegate = self
-
-    // Set delegate AFTER class initialized
-    Messaging.messaging().delegate = self
-
-    UNUserNotificationCenter.current().requestAuthorization(
-      options: [.alert, .sound, .badge]
-    ) { granted, error in
-      if let error = error {
-        print("Auth Error: \(error)")
-      } else {
-        print("Permission: \(granted)")
-      }
-    }
-
-    application.registerForRemoteNotifications()
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // MARK: - APNs Token
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-}
-
-// MARK: - Messaging Delegate
-extension AppDelegate: MessagingDelegate {
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    if let token = fcmToken {
-      print("FCM Token: \(token)")
-    }
-  }
-}
-
-// MARK: - Notifications Delegate
-extension AppDelegate: UNUserNotificationCenterDelegate {
-  func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    willPresent notification: UNNotification,
-    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-  ) {
-    completionHandler([.alert, .sound, .badge])
-  }
-
-  func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    didReceive response: UNNotificationResponse,
-    withCompletionHandler completionHandler: @escaping () -> Void
-  ) {
-    completionHandler()
   }
 }
